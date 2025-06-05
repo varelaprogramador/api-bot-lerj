e14c2ccf26d76ac18e97ba76dacc1ea7d207c3b5
Ryan varela <108085059+varelaprogramador@users.noreply.github.com>
2025-06-05 09:27:02 -0300

    Atualiza as consultas para garantir que a busca por nomes de combos e produtos seja feita em letras minúsculas, melhorando a consistência e a precisão das pesquisas.

--- src/routes/payments/index.ts
+++ src/routes/payments/index.ts
@@ -58,7 +58,7 @@ export default async function (app: FastifyInstance) {
         const { data: combos, error: comboError } = await supabase
           .from("bot_conversa_com_combos")
           .select("*")
-          .ilike("nome_combo", dadosProcessados.produto.nome);
+          .ilike("nome_combo", dadosProcessados.produto.nome.toLowerCase());
 
         console.log("Combos encontrados:", combos);
         if (comboError || !combos || combos.length === 0) {
@@ -133,7 +133,7 @@ export default async function (app: FastifyInstance) {
         const { data: produtos, error: produtoError } = await supabase
           .from("bot_conversa_com_produto")
           .select("*")
-          .ilike("nome", dadosProcessados.produto.nome);
+          .ilike("nome", dadosProcessados.produto.nome.toLowerCase());
 
         if (produtoError || !produtos || produtos.length === 0) {
           console.error(
